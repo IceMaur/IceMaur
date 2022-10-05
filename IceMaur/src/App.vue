638dd94fb0aac31ea066b87e4308a18ca0ab4c9e
MauriceTulp <maurice.tulp@munit-solutions.com>
2022-10-05 16:30:37 +0200

    Init Vue

---
+++ IceMaur/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Link from './components/Link.vue';
+</script>
+
+<template>
+  <Link href="https://github.com/IceMaur/IceMaur">IceMaur</Link>
+</template>
+
+<style scoped>
+</style>
