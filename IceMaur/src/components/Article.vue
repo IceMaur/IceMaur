99664bd87679a214d6691a20534768a8fb30cf9c
MauriceTulp <maurice.tulp@munit-solutions.com>
2022-10-10 11:40:55 +0200

    Blog Detail new style and image

--- IceMaur/src/components/Article.vue
+++ IceMaur/src/components/Article.vue
@@ -1,6 +1,9 @@
 <template>
-    <h1>{{article.fields.title}}</h1>
-    <RichTextRenderer :document="article.fields.content" :nodeRenderers="null" :markRenderers="null" />
+    <img class="article-image" :alt="article.image.fields.title" :src="article.image.fields.file.url" />
+    <div class="article-content">
+        <h1>{{article.title}}</h1>
+        <RichTextRenderer :document="article.content" :nodeRenderers="null" :markRenderers="null" />
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -15,5 +18,24 @@ const articles = await ContentfulClient.getEntries<Article>({
     content_type: 'article',
     'fields.title': title
 });
-const article = articles.items[0];
+const article = articles.items[0].fields;
 </script>
+
+<style scoped lang="less">
+.article-image {
+    width: 100vw;
+    object-fit: cover;
+    max-height: 60vh;
+    margin-top: -0.25rem;
+    margin-left: 50%;
+    transform: translateX(-50%);
+}
+
+.article-content {
+    @media only screen and (min-width: 1200px) {
+        max-width: 48rem;
+        margin-left: auto;
+        margin-right: auto;
+    }
+}
+</style>
