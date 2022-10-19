3ef62d81182721173cfe51358dba37bf0d7fb569
MauriceTulp <maurice.tulp@munit-solutions.com>
2022-10-19 13:57:44 +0200

    RadioBlockButton component

---
+++ IceMaur/src/components/buttons/RadioBlockButton.vue
@@ -0,0 +1,43 @@
+<template>
+    <input type="radio" :id="`appearance-${value}`" :value="value" v-model="settingsStore.appearance" />
+    <label :for="`appearance-${value}`"><slot></slot></label>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from 'vue';
+import { settingsStore } from '../../store/settings.js'
+
+interface Props {
+    value: string
+}
+
+const props = withDefaults(defineProps<Props>(), { value: ""});
+const { value } = toRefs(props);
+</script>
+
+<style scoped lang="less">
+    input[type="radio"] {
+        display: none;
+
+        &+ label {
+            cursor: pointer;
+            margin-right: 1rem;
+            padding: 0.5rem;
+            background: var(--color-main);
+            color: var(--color-secondary);
+        }
+    }
+
+    input:checked + label {
+        background: var(--color-tertiary);
+        color: var(--color-main);
+    }
+
+    #appearance-spotify:checked + label {
+        background: var(--color-spotify-primary);
+    }
+
+    #appearance-premium:checked + label {
+        background: radial-gradient(ellipse farthest-corner at right bottom, #FEDB37 0%, #FDB931 8%, #9f7928 30%, #8A6E2F 40%, transparent 80%), radial-gradient(ellipse farthest-corner at left top, #FFFFFF 0%, #FFFFAC 8%, #D1B464 25%, #5d4a1f 62.5%, #5d4a1f 100%);
+    }
+</style>
--- IceMaur/src/pages/Settings.vue
+++ IceMaur/src/pages/Settings.vue
@@ -1,43 +1,12 @@
 <template>
     <h1>Settings</h1>
     <h2>appearance</h2>
-    <input type="radio" id="appearance-default" value="" v-model="settingsStore.appearance" />
-    <label for="appearance-default">Default</label>
-    <input type="radio" id="appearance-dark" value="dark" v-model="settingsStore.appearance" />
-    <label for="appearance-dark">Dark</label>
-    <input type="radio" id="appearance-spotify" value="spotify" v-model="settingsStore.appearance" />
-    <label for="appearance-spotify">Spotify</label>
-    <input type="radio" id="appearance-premium" value="author-page" v-model="settingsStore.appearance" />
-    <label for="appearance-premium">Premium</label>
+    <RadioBlockButton>Default</RadioBlockButton>
+    <RadioBlockButton value="dark">Dark</RadioBlockButton>
+    <RadioBlockButton value="spotify">Spotify</RadioBlockButton>
+    <RadioBlockButton value="author-page">Premium</RadioBlockButton>
 </template>
 
 <script setup lang="ts">
-import { settingsStore } from '../store/settings.js'
+import RadioBlockButton from '../components/buttons/RadioBlockButton.vue';
 </script>
-
-<style scoped lang="less">
-    input[type="radio"] {
-        display: none;
-
-        &+ label {
-            cursor: pointer;
-            margin-right: 1rem;
-            padding: 0.5rem;
-            background: var(--color-main);
-            color: var(--color-secondary);
-        }
-    }
-
-    input:checked + label {
-        background: var(--color-tertiary);
-        color: var(--color-main);
-    }
-
-    #appearance-spotify:checked + label {
-        background: var(--color-spotify-primary);
-    }
-
-    #appearance-premium:checked + label {
-        background: radial-gradient(ellipse farthest-corner at right bottom, #FEDB37 0%, #FDB931 8%, #9f7928 30%, #8A6E2F 40%, transparent 80%), radial-gradient(ellipse farthest-corner at left top, #FFFFFF 0%, #FFFFAC 8%, #D1B464 25%, #5d4a1f 62.5%, #5d4a1f 100%);
-    }
-</style>
