99753a860ca94f020e76ebf6b316507e3a3f1849
MauriceTulp <maurice.tulp@munit-solutions.com>
2022-10-19 13:59:44 +0200

    AppearanceBlockButton component (rename component)

--- IceMaur/src/pages/Settings.vue
+++ IceMaur/src/pages/Settings.vue
@@ -1,12 +1,12 @@
 <template>
     <h1>Settings</h1>
     <h2>appearance</h2>
-    <RadioBlockButton>Default</RadioBlockButton>
-    <RadioBlockButton value="dark">Dark</RadioBlockButton>
-    <RadioBlockButton value="spotify">Spotify</RadioBlockButton>
-    <RadioBlockButton value="author-page">Premium</RadioBlockButton>
+    <AppearanceBlockButton>Default</AppearanceBlockButton>
+    <AppearanceBlockButton value="dark">Dark</AppearanceBlockButton>
+    <AppearanceBlockButton value="spotify">Spotify</AppearanceBlockButton>
+    <AppearanceBlockButton value="author-page">Premium</AppearanceBlockButton>
 </template>
 
 <script setup lang="ts">
-import RadioBlockButton from '../components/buttons/RadioBlockButton.vue';
+import AppearanceBlockButton from '../components/buttons/AppearanceBlockButton.vue';
 </script>
