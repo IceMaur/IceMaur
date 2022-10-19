346ef76f141ee1896d4ae2e029e158009a25988f
MauriceTulp <maurice.tulp@munit-solutions.com>
2022-10-19 13:44:33 +0200

    default input style

--- IceMaur/src/pages/Settings.vue
+++ IceMaur/src/pages/Settings.vue
@@ -28,19 +28,16 @@ import { settingsStore } from '../store/settings.js'
         }
     }
 
-    #appearance-default:checked + label,
-    #appearance-dark:checked + label {
+    input:checked + label {
         background: var(--color-tertiary);
         color: var(--color-main);
     }
 
     #appearance-spotify:checked + label {
         background: var(--color-spotify-primary);
-        color: var(--color-main);
     }
 
     #appearance-premium:checked + label {
         background: radial-gradient(ellipse farthest-corner at right bottom, #FEDB37 0%, #FDB931 8%, #9f7928 30%, #8A6E2F 40%, transparent 80%), radial-gradient(ellipse farthest-corner at left top, #FFFFFF 0%, #FFFFAC 8%, #D1B464 25%, #5d4a1f 62.5%, #5d4a1f 100%);
-        color: var(--color-main);
     }
 </style>
