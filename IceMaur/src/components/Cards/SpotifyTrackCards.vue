181fe550a9a365732dec95f71383440a750b3b04
MauriceTulp <maurice.tulp@munit-solutions.com>
2022-10-24 09:29:28 +0200

    Other spotify tracks

--- IceMaur/src/components/Cards/SpotifyTrackCards.vue
+++ IceMaur/src/components/Cards/SpotifyTrackCards.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="spotify-track-cards">
-        <BrandCard v-for="spotifyTrack in spotifyTracks.items" class="spotify-track-item" title="Spotify">
+        <BrandCard v-for="spotifyTrack in trackWithoutCurrentTrack" class="spotify-track-item" title="Spotify">
             <SpotifyTrackIframe class="spotify-track-frame" :trackId="spotifyTrack.fields.id"></SpotifyTrackIframe>
             <SpotifyTrackButton type="secondary" :trackId="spotifyTrack.fields.id">{{spotifyTrack.fields.name}}</SpotifyTrackButton>
         </BrandCard>
@@ -8,15 +8,25 @@
 </template>
 
 <script setup lang="ts">
+import { toRefs } from 'vue';
 import ContentfulClient from '../../data/ContentfulClient';
 import SpotifyTrack from '../../objects/SpotifyTrack';
 import BrandCard from '../../components/cards/BrandCard.vue';
 import SpotifyTrackIframe from '../../components/iFrames/SpotifyTrackIframe.vue';
 import SpotifyTrackButton from '../buttons/SpotifyTrackButton.vue';
 
+interface Props {
+    trackId?: string,
+}
+
+const props = defineProps<Props>();
+const { trackId } = toRefs(props);
+
 const spotifyTracks = await ContentfulClient.getEntries<SpotifyTrack>({
     content_type: 'spotifyTrack',
 });
+
+const trackWithoutCurrentTrack = spotifyTracks.items.filter(t => t.fields.id !== trackId?.value);
 </script>
 
 <style scoped lang="less">
